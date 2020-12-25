60d53d98e1aab44be46d9897d516b7edb530d839
Hugo Iñigo <hinigo001@ikasle.ehu.eus>
2020-12-26 00:33:25 +0100

    More fixes to autoconf.sh

--- scripts/.local/bin/autoconf.sh
+++ scripts/.local/bin/autoconf.sh
@@ -26,41 +26,40 @@ destdir="$home/.local/builds"
 # Helper functions to install custom programs
 GitInst () {
     gitname="$(basename "$1" .git)"
-    gitdir="$([ "$3" ] && echo "$3" || echo "$destdir")/$gitdir"
-    gitbranch=$([ "$2" ] && echo "$2" || echo "$branch")
-    printf "Installing %s " "$gitname"
+    gitdir="$([ "$3" ] && echo "$3" || echo "$destdir")/$gitname"
+    gitbranch=$([ "$2" ] && echo "$2")
+    printf "Installing %s" "$gitname"
     git clone "$1" "$gitdir" >/dev/null 2>&1
     cd "$gitdir" || { Warning "Could not clone $gitname."; return 1; }
-    git checkout "$gitbranch" >/dev/null 2>&1\
-        || Warning "\nCould not change to branch $gitbranch. Trying to install from master."
+    [ -n "$gitbranch" ] && (git checkout "$gitbranch" >/dev/null 2>&1\
+        || Warning "Could not change to branch $gitbranch. Trying to install from master.")
     { make clean >/dev/null 2>&1 ; make install >/dev/null 2>&1; }\
-        || Warning "\nCould not install $gitname"\
-        && printf "Done.\n"
+        || Warning "Could not install $gitname"\
+        && printf " done.\n"
     return 0
 }
 
 SetupDotfiles () {
     gitname="$(basename "$1" .git)"
-    gitdir="$([ "$3" ] && echo "$3" || echo "$destdir")/$gitdir"
-    gitbranch=$([ "$2" ] && echo "$2" || echo "$branch")
-    printf "Setting up dotfiles %s " "$gitname"
+    gitdir="$([ "$3" ] && echo "$3" || echo "$destdir")/$gitname"
+    gitbranch=$([ "$2" ] && echo "$2")
+    printf "Setting up dotfiles %s" "$gitname"
     git clone "$1" "$gitdir" >/dev/null 2>&1 
+    [ -f "$home/.bashrc" ] && rm "$home/.bashrc"
+    [ -f "$home/.profile" ] && rm "$home/.profile"
     cd "$gitdir" || Error
-    git checkout "$gitbranch" >/dev/null 2>&1\
-        || Warning "\nCould not change to branch $gitbranch. Trying to install from master."
+    [ -n "$gitbranch" ] && (git checkout "$gitbranch" >/dev/null 2>&1\
+        || Warning "Could not change to branch $gitbranch. Trying to install from master.")
+    [ -f "$gitdir/README.md" ] && rm "$gitdir/README.md"
     [ "$(command -v stow)" ] \
-        && stow -- *glob*\
-        || Warning "\nCould not set up dotfiles $gitname"; return 1
-    printf "Done.\n"
+        && stow -v -t "$home" *\
+        || Warning "Could not set up dotfiles $gitname"; return 1
+    printf " done.\n"
 }
 
 ## BEGINNING OF THE SCRIPT
 # Try to install sudo
 apt install sudo >/dev/null 2>&1 || Error "Are you su and have an internet connection?"
-
-Error
-echo "Exiting..."
-exit 0
 
 # Setup the XDG Base Directory specification
 sed -i '/^else$/a\
@@ -80,8 +79,10 @@ sudo adduser "$user" sudo
 sudo adduser "$user" wheel
 
 # Install some basic programs from apt
+printf "Updating apt sources"
 apt-get update >/dev/null 2>&1
-apt-get install\
+echo "Done."
+apt-get install -y \
 	build-essential\
 	firmware-iwlwifi\
 	network-manager\
@@ -114,7 +115,6 @@ apt-get install\
 	redshift\
 	xdotool\
 	sysstat\
-	calc\
 	zathura\
 	mpv\
 	thunar\
@@ -126,7 +126,6 @@ apt-get install\
 	info\
 	htop\
         libxinerama-dev\
-        >/dev/null 2>&1
 
 # Update fonts list
 fc-cache
@@ -146,7 +145,7 @@ sudo systemctl enable NetworkManager
 echo "Done."
 printf "Reboot now? (Required to enable wifi) [y/n] "
 read -r confirm
-\( [ -z "$confirm" ] || [ "$confirm" != "n" ] \) \
-	&& { echo "Rebooting now..."; reboot now; }\
+[ -z "$confirm" -o "$confirm" != "n" ] \
+	&& { echo "Rebooting now..."; sudo reboot now; }\
 	|| { echo "Log back in for changes to take effect."; }
 exit 0
